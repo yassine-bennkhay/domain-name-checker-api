fea64989660ea69f61aef1db8ce08f07c1abf59a
Yassine <benkhayyassine8@gmail.com>
2023-03-08 23:49:10 +0100

    some changes

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
 require("dotenv").config();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 app.use("/", require("./routes/apiRoute"));
 app.listen(PORT, () => console.log(`Server is Running on the Port ${PORT}`));
